f7671ac865d7a7c6e24f2f5e352860be48b507e9
María García Cáceres <Mara Garca Cceres@192.168.1.173>
2020-11-15 14:15:50 +0100

    Añadido clase Producto y Carta(sin terminar)

---
+++ src/main/java/org/springframework/samples/petclinic/model/Producto.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@MappedSuperclass
+public class Producto extends BaseEntity {
+	
+	@Column(name = "nombre")
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String nombre;
+
+	@Column(name = "coste")
+	@NotNull
+	@Min(0)
+	@Max(100)
+	private Integer coste;
+	
+	@Column(name = "contador")
+	@NotNull
+	private Integer contador;
+
+}
