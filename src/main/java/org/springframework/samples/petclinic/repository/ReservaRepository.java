75f2763c405cf240a5a39751262544492db21d5b
Raúl Parrado Gordón <raul-pg@hotmail.com>
2020-11-21 12:05:50 +0100

    Reserva se habia borrado del main lol

---
+++ src/main/java/org/springframework/samples/petclinic/repository/ReservaRepository.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.samples.petclinic.model.Reserva;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends CrudRepository<Reserva, Integer>{
+
+
+//    //filtrar Reserva 
+//    @Query("SELECT DISTINCT nombreCliente FROM Reserva JOIN cliente WHERE cliente.reserva = :reserva_id%")
+//    public Collection<Reserva> findByNombreCliente(@Param("nombreCliente") String nombre_cliente);
+
+    List<Reserva> findAll() throws DataAccessException;
+
+    Reserva findReservaById(int reserva_id) throws DataAccessException;
+
+
+
+}
