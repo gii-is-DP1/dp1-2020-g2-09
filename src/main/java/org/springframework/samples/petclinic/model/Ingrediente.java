9cc968d24f01ee81b90e845617ee5093cf6f108a
María García Cáceres <Mara Garca Cceres@192.168.1.173>
2020-11-28 17:26:49 +0100

    imports

--- src/main/java/org/springframework/samples/petclinic/model/Ingrediente.java
+++ src/main/java/org/springframework/samples/petclinic/model/Ingrediente.java
@@ -1,60 +1,11 @@
-
 package org.springframework.samples.petclinic.model;
 
 import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
-
-import javax.validation.constraints.Size;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
-import com.sun.istack.NotNull;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper=false)
-@Entity
-public class Ingrediente extends BaseEntity {
-	
-	@Column(name = "nombre")
-	@NotNull
-	@Size(min = 2, max = 20)
-	private String nombre;
-	
-	@Column(name = "tipo")
-	@NotNull
-	private String tipo;
-	
-	@Column(name = "fechaCaducidad")        
-	@DateTimeFormat(pattern = "yyyy/MM/dd")
-	private LocalDate fechaCaducidad;
-
-	@ManyToOne
-	@JoinColumn(name = "Alergenos")
-	
-	private Alergenos alergenos;
-
-	
-=======
-package org.springframework.samples.petclinic.model;
-
-import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
-
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
