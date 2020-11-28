614c9497ada00086135a575d406e9580608273b9
Raúl Parrado Gordón <raul-pg@hotmail.com>
2020-11-28 20:49:40 +0100

    Validación de carta completa

--- src/main/java/org/springframework/samples/petclinic/web/CartaValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/CartaValidator.java
@@ -1,19 +1,40 @@
 package org.springframework.samples.petclinic.web;
 
+import java.time.LocalDate;
+
+import org.springframework.samples.petclinic.model.Carta;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
 public class CartaValidator implements Validator {
+	private static final String REQUIRED = "required";
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
-		return false;
+		return Carta.class.isAssignableFrom(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		Carta carta = (Carta) target;
+		String name = carta.getNombre();
+		// name validation
+		if (!StringUtils.hasLength(name) || name.length()>50 || name.length()<0) {
+			errors.rejectValue("Nombre", REQUIRED+"debe estar entre 3 y 50 caracteres", REQUIRED+" debe estar entre 3 y 50 caracteres");
+		}
+
+		// NOMBRE validation
+		if (carta.getNombre() == null) {
+			errors.rejectValue("Nombre", REQUIRED,REQUIRED);
+		}
+
+		// FECHA validation
+		if (carta.getFecha() == null || carta.getFecha().isAfter(LocalDate.now())) {
+			errors.rejectValue("fecha", REQUIRED+"debe ser antes de la fecha actual",REQUIRED);
+		}		
 		
 	}
 
