933aef6aaf94627f473a761a08d7b95cf9cf605a
Jesús Roldán Cadena <jesrolcad@alum.us.es>
2020-12-14 20:35:33 +0100

    Pruebas del controlador de reclamación (no funcionan)
    Pruebas de reclamación formatter (estas sí funcionan)

---
+++ src/main/java/org/springframework/samples/petclinic/web/ReclamacionFormatter.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.web;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.samples.petclinic.model.Reclamacion;
+import org.springframework.samples.petclinic.service.ReclamacionService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReclamacionFormatter implements Formatter<Reclamacion>{
+private final ReclamacionService rs;
+	
+	@Autowired
+	public ReclamacionFormatter(ReclamacionService reclamacionService) {
+		this.rs = reclamacionService;
+	}
+	@Override
+	public String print(Reclamacion object, Locale locale) {
+		return object.getObservacion();
+//				"La reclamacion con id "+ object.getId()+ " sobre el pedido " + object.getPedido() + 
+//				" en la fecha "+ object.getFechaReclamacion()+ 
+//				" contiene la siguiente observación: "+ object.getObservacion();
+	}
+
+	@Override
+	public Reclamacion parse(String text, Locale locale) throws ParseException {
+		Collection<Reclamacion> findReclamacion = this.rs.findReclamaciones();
+		for(Reclamacion r:findReclamacion) {
+			if(r.getObservacion().equals(text)) {
+				return r;
+			}
+		}
+		throw new ParseException("Reclamacion not found: " + text, 0);
+	}
+}
---
+++ src/test/java/org/springframework/samples/petclinic/web/ReclamacionControllerTests.java
@@ -0,0 +1,152 @@
+package org.springframework.samples.petclinic.web;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDate;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Cliente;
+import org.springframework.samples.petclinic.model.Pedido;
+import org.springframework.samples.petclinic.model.Reclamacion;
+import org.springframework.samples.petclinic.service.PedidoService;
+import org.springframework.samples.petclinic.service.ReclamacionService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(value = ReclamacionController.class,
+includeFilters = @ComponentScan.Filter(value = ReclamacionFormatter.class, type = FilterType.ASSIGNABLE_TYPE),
+excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
+excludeAutoConfiguration= SecurityConfiguration.class)
+class ReclamacionControllerTests {
+
+	private static final int TEST_RECLAMACION_ID = 9;
+
+	private static final int TEST_PEDIDO_ID = 99;
+
+	@Autowired
+	private ReclamacionController reclamacionController;
+
+
+	@MockBean
+	private ReclamacionService reclamacionService;
+        
+    @MockBean
+	private PedidoService pedidoService;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setup() {
+		Reclamacion r = new Reclamacion();
+		Pedido p = new Pedido();
+		
+		Cliente cliente = new Cliente();
+		cliente.setApellidos("Roldán Cadena");
+		cliente.setEmail("jrc@gmail.com");
+		cliente.setFechaNacimiento(LocalDate.of(2000, 6,7));
+		cliente.setId(1);
+		cliente.setNombre("Jesús");
+		cliente.setTelefono(123456789);
+		
+		
+		p.setCliente(cliente);
+		p.setDireccion("Bda San Diego");
+		//p.setEstadoPedido(estadoPedido);
+		p.setFechaPedido(LocalDate.of(2020, 12, 1));
+		p.setId(1);
+		//p.setOfertasEnPedido(ofertasEnPedido);
+		p.setPrecio(40.5);
+		//p.setTipoEnvio(tipoEnvio);
+		//p.setTipoPago(tipoPago);
+		
+		
+		r.setId(3);
+		r.setFechaReclamacion(LocalDate.of(2020, 11, 24));
+		r.setObservacion("aaaaa");
+		
+		given(this.reclamacionService.findReclamaciones()).willReturn(Lists.newArrayList(r));
+		given(this.pedidoService.findPedidoById(TEST_PEDIDO_ID)).willReturn(new Pedido());
+		given(this.reclamacionService.findReclamacionById(TEST_RECLAMACION_ID)).willReturn(new Reclamacion());
+	}
+
+	@WithMockUser(value = "spring")
+        @Test
+	void testInitCreationForm() throws Exception {
+		mockMvc.perform(get("/reclamaciones/new")).andExpect(status().isOk()).andExpect(model().attributeExists("reclamacion"))
+				.andExpect(view().name("reclamaciones/createOrUpdateReclamacionForm"));
+	}
+
+	@WithMockUser(value = "spring")
+        @Test
+	void testProcessCreationFormSuccess() throws Exception {
+		mockMvc.perform(post("/pedidos/{pedidoId}/reclamaciones/new", TEST_PEDIDO_ID)
+				.with(csrf())
+				.param("fechaReclamacion", "2020/11/24")
+				.param("observacion", "aaaaa"))
+	.andExpect(status().is3xxRedirection())
+	.andExpect(view().name("redirect:/reclamaciones/{reclamacionId}"));
+}
+
+//	@WithMockUser(value = "spring")
+//    @Test
+//	void testProcessCreationFormHasErrors() throws Exception {
+//		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID)
+//							.with(csrf())
+//							.param("name", "Betty")
+//							.param("birthDate", "2015/02/12"))
+//				.andExpect(model().attributeHasNoErrors("owner"))
+//				.andExpect(model().attributeHasErrors("pet"))
+//				.andExpect(status().isOk())
+//				.andExpect(view().name("pets/createOrUpdatePetForm"));
+//	}
+
+//    @WithMockUser(value = "spring")
+//	@Test
+//	void testInitUpdateForm() throws Exception {
+//		mockMvc.perform(get("/reservas/{reservaId}/mesas/{mesaId}/edit", TEST_RESERVA_ID, TEST_MESA_ID))
+//				.andExpect(status().isOk()).andExpect(model().attributeExists("mesa"))
+//				.andExpect(view().name("mesas/createOrUpdateMesaForm"));
+//	}
+//    
+//    @WithMockUser(value = "spring")
+//	@Test
+//	void testProcessUpdateFormSuccess() throws Exception {
+//		mockMvc.perform(post("/reservas/{reservaId}/mesas/{mesaId}/edit", TEST_RESERVA_ID, TEST_MESA_ID)
+//							.with(csrf())
+//							.param("numeroPersonas", "6")
+//							.param("tipo_reserva", "CENA")
+//							.param("fecha_reserva", "2015/02/12"))
+//				.andExpect(status().is3xxRedirection())
+//				.andExpect(view().name("redirect:/reservas/{reservaId}"));
+//	}
+//    
+//    @WithMockUser(value = "spring")
+//	@Test
+//	void testProcessUpdateFormHasErrors() throws Exception {
+//		mockMvc.perform(post("/reservas/{reservaId}/mesas/{mesaId}/edit", TEST_RESERVA_ID, TEST_MESA_ID)
+//							.with(csrf())
+//							.param("numeroPersonas", "6")
+//							.param("tipo_reserva", "CENA")
+//							.param("fecha_reserva", "2015/02/12"))
+//				.andExpect(model().attributeHasNoErrors("reserva"))
+//				.andExpect(model().attributeHasErrors("mesa")).andExpect(status().isOk())
+//				.andExpect(view().name("mesas/createOrUpdatePetForm"));
+//	}
+	
+
+}
