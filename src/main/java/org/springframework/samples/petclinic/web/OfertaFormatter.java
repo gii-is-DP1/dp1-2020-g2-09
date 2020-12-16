26941ffe9a4d765bc2ede28cd46a3e03bafac4b4
Jesús Roldán Cadena <jesrolcad@alum.us.es>
2020-12-16 22:01:11 +0100

    Pruebas OfertaController (hay que revisarlas) y OfertaFormatter

---
+++ src/main/java/org/springframework/samples/petclinic/web/OfertaFormatter.java
@@ -0,0 +1,49 @@
+package org.springframework.samples.petclinic.web;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.samples.petclinic.model.Oferta;
+import org.springframework.samples.petclinic.service.OfertaService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfertaFormatter implements Formatter<Oferta>{
+private final OfertaService rs;
+	
+	@Autowired
+	public OfertaFormatter(OfertaService ofertaService) {
+		this.rs = ofertaService;
+	}
+	@Override
+	public String print(Oferta object, Locale locale) {
+		return String.valueOf(object.getId());
+//			"La oferta con id "+ object.getId()+ " con coste " + object.getCoste() + 
+//			" comienza el "+ object.getFechaInicial() " y acaba el " + object.getFechaFinal() 
+//		" .Tiene el nivel de socio "+ object.getNivelSocio() " y tamaño " + object.getTamanoOferta();
+	}
+
+	@Override
+	public Oferta parse(String text, Locale locale) throws ParseException {
+		Collection<Oferta> findOferta = this.rs.findOfertas();
+		for(Oferta o:findOferta) {
+			if(String.valueOf(o.getId()).equals(text)) {
+				return o;
+			}
+		}
+		throw new ParseException("Oferta not found: " + text, 0);
+	}
+	
+	public Oferta parse2(Integer i, Locale locale) throws ParseException {
+		Collection<Oferta> findOferta = this.rs.findOfertas();
+		for(Oferta o:findOferta) {
+			if(o.getId()==i) {
+				return o;
+			}
+		}
+		throw new ParseException("Oferta not found: " + i, 0);
+	}
+}
