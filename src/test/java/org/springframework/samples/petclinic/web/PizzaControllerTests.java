aeba5824cd4afaeb1aa34c3816db0b0c76b69d28
María García Cáceres <Mara Garca Cceres@192.168.1.173>
2020-12-17 13:33:55 +0100

    mesa controller test e intento de pizza test

--- src/test/java/org/springframework/samples/petclinic/web/PizzaControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/PizzaControllerTests.java
@@ -10,6 +10,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,8 +24,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Alergenos;
 import org.springframework.samples.petclinic.model.Carta;
 import org.springframework.samples.petclinic.model.Cliente;
+import org.springframework.samples.petclinic.model.Ingrediente;
 import org.springframework.samples.petclinic.model.Oferta;
 import org.springframework.samples.petclinic.model.Pedido;
 import org.springframework.samples.petclinic.model.Pizza;
@@ -50,11 +56,10 @@ class PizzaControllerTests {
 	private static final int TEST_CARTA_ID = 1;
 	private static final int TEST_OFERTA_ID = 1;
 	private static final int TEST_CLIENTE_ID = 1;
-	private static final int TEST_INGREDIENTE_ID = 1;
 	
 
 	@Autowired
-	private PizzaController otrosController;
+	private PizzaController pizzaController;
 
 
 	@MockBean
@@ -80,7 +85,24 @@ class PizzaControllerTests {
 		pizza1.setCoste(12);
 		pizza1.setNombre("Barbacoa");
 		pizza1.setContador(1);
-		//pizza1.setIngredientes(ingredientes);
+		
+		Ingrediente ingrediente1 = new Ingrediente();
+		Alergenos alergeno1 = new Alergenos();
+		
+		alergeno1.setName("contiene lactosa");
+		alergeno1.setId(55);
+		
+		ingrediente1.setAlergenos(alergeno1);
+		ingrediente1.setFechaCaducidad(LocalDate.of(2021, 12, 05));
+		ingrediente1.setId(55);
+		ingrediente1.setNombre("tomate");
+		ingrediente1.setTipo("contiene lácteos");
+
+		List<Ingrediente> lista_ingredientes = new ArrayList<Ingrediente>();
+		lista_ingredientes.add(ingrediente1);
+
+		
+		pizza1.setIngredientes(lista_ingredientes);
 		
 		TamanoProducto t=new TamanoProducto();
 		t.setId(66);
@@ -94,296 +116,101 @@ class PizzaControllerTests {
 		
 		given(this.pizzaService.findPizzas()).willReturn(Lists.newArrayList(pizza1));
 		given(this.pizzaService.findPizzaById(TEST_PIZZA_ID)).willReturn(new Pizza());
-		//given(this.pizzaService.findIdPizzaById(TEST_CARTA_ID)).willReturn(new ArrayList<Integer>());
+		given(this.pizzaService.findIdPizzaById(TEST_CARTA_ID)).willReturn(new ArrayList<Integer>());
 		
 		given(this.pedidoService.findPedidoById(TEST_PEDIDO_ID)).willReturn(new Pedido());
 		given(this.cartaService.findCartaById(TEST_CARTA_ID)).willReturn(new Carta());
 		given(this.ofertaService.findOfertaById(TEST_OFERTA_ID)).willReturn(new Oferta());
 		given(this.clienteService.findCuentaById(TEST_CLIENTE_ID)).willReturn(new Cliente());
-		//given(this.ingredienteService.findIngredienteById(TEST_OFERTA_ID)).willReturn(new Ingrediente());
+		given(this.ingredienteService.findIngredienteById(TEST_OFERTA_ID)).willReturn(new Ingrediente());
 	}
 
 	@WithMockUser(value = "spring")
         @Test
 	void testInitCreationForm() throws Exception {
-		mockMvc.perform(get("/ofertas/{ofertaId}/ofertas/new", TEST_OFERTA_ID)).andExpect(status().isOk())
-				.andExpect(view().name("ofertas/createOrUpdateOfertaForm")).andExpect(model().attributeExists("Pizza"));
+		mockMvc.perform(get("/pizzas/new"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"))
+				.andExpect(model().attributeExists("pizza"));
 		
-		mockMvc.perform(get("/pedidos/{pedidoId}/pedidos/new", TEST_PEDIDO_ID)).andExpect(status().isOk())
-				.andExpect(view().name("pedidos/createOrUpdatePedidoForm")).andExpect(model().attributeExists("Pizza"));
-		
-		mockMvc.perform(get("/cartas/{cartaId}/cartas/new", TEST_CARTA_ID)).andExpect(status().isOk())
-				.andExpect(view().name("cartas/createOrUpdateCartaForm")).andExpect(model().attributeExists("Pizza"));
-		
-		mockMvc.perform(get("/clientes/{clienteId}/clientes/new", TEST_CLIENTE_ID)).andExpect(status().isOk())
-				.andExpect(view().name("clientes/createOrUpdateClienteForm")).andExpect(model().attributeExists("Pizza"));
-
-		/*mockMvc.perform(get("/ingredientes/{ingredienteId}/ingredientes/new", TEST_INGREDIENTE_ID)).andExpect(status().isOk())
-				.andExpect(view().name("ingredientes/createOrUpdateIngredienteForm")).andExpect(model().attributeExists("Pizza"));*/
 	}
 
 	@WithMockUser(value = "spring")
         @Test
 	void testProcessCreationFormSuccess() throws Exception {
-		mockMvc.perform(post("/ofertas/{ofertaId}/pizzas/new", TEST_OFERTA_ID)
+		mockMvc.perform(post("/pizzas/new")
 							.with(csrf())
 							.param("contador", "2")
 							.param("coste", "13")
 							.param("nombre", "Pizza2")
 							.param("tamano", "mini")
-							.param("tipoMasa", "extrafina"))
+							.param("tipoMasa", "extrafina")
+							.param("ingredientes", "tomate"))
 				.andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/ofertas/{ofertaId}"));
+				.andExpect(view().name("redirect:/allPizzas"));
 		
-		mockMvc.perform(post("/pedidos/{pedidoId}/pizzas/new", TEST_PEDIDO_ID)
-						.with(csrf())
-						.param("contador", "2")
-						.param("coste", "13")
-						.param("nombre", "Pizza2")
-						.param("tamano", "mini")
-						.param("tipoMasa", "extrafina"))
-				.andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/pedidos/{pedidoId}"));
-		
-		mockMvc.perform(post("/cartas/{cartaId}/pizzas/new", TEST_CARTA_ID)
-						.with(csrf())
-						.param("contador", "2")
-						.param("coste", "13")
-						.param("nombre", "Pizza2")
-						.param("tamano", "mini")
-						.param("tipoMasa", "extrafina"))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/cartas/{cartaId}"));
-		
-		mockMvc.perform(post("/cuentas/{cuentaId}/pizzas/new", TEST_CLIENTE_ID)
-					.with(csrf())
-					.param("contador", "2")
-					.param("coste", "13")
-					.param("nombre", "Pizza2")
-					.param("tamano", "mini")
-					.param("tipoMasa", "extrafina"))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/cuentas/{cuentaId}"));
 
-		/*mockMvc.perform(post("/ingredientes/{ingredienteId}/pizzas/new", TEST_INGREDIENTE_ID)
-					.with(csrf())
-					.param("contador", "2")
-					.param("coste", "13")
-					.param("nombre", "Pizza2")
-					.param("tamano", "mini")
-					.param("tipoMasa", "extrafina"))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/ingredientes/{ingredienteId}"));*/
 	}
 
 	@WithMockUser(value = "spring")
     @Test
 	void testProcessCreationFormHasErrors() throws Exception {
-		mockMvc.perform(post("/ofertas/{ofertaId}/pizzas/{pizzaId}/edit", TEST_OFERTA_ID, TEST_PIZZA_ID)
+		mockMvc.perform(post("/pizzas/{pizzaId}/new", TEST_PIZZA_ID)
 							.with(csrf())
-							.param("contador", "2")
+							.param("contador", "ññññ")
 							.param("coste", "13")
 							.param("nombre", "Pizza2")
 							.param("tamano", "mini")
-							.param("tipoMasa", "extrafina"))
-				.andExpect(model().attributeHasNoErrors("oferta"))
-				.andExpect(model().attributeHasErrors("Pizza"))
+							.param("tipoMasa", "extrafina")
+							.param("ingredientes", "tomate"))
+
+				.andExpect(model().attributeHasErrors("pizza"))
 				.andExpect(status().isOk())
+				.andExpect(status().is3xxRedirection())
 				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/pedidos/{pedidoId}/pizzas/{pizzaId}/edit", TEST_PEDIDO_ID, TEST_PIZZA_ID)
-							.with(csrf())
-							.param("contador", "2")
-							.param("coste", "13")
-							.param("nombre", "Pizza2")
-							.param("tamano", "mini")
-							.param("tipoMasa", "extrafina"))
-				.andExpect(model().attributeHasNoErrors("pedido"))
-				.andExpect(model().attributeHasErrors("Pizza"))
-				.andExpect(status().isOk())
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/cartas/{cartaId}/pizzas/{pizzaId}/edit", TEST_CARTA_ID, TEST_PIZZA_ID)
-							.with(csrf())
-							.param("contador", "2")
-							.param("coste", "13")
-							.param("nombre", "Pizza2")
-							.param("tamano", "mini")
-							.param("tipoMasa", "extrafina"))
-				.andExpect(model().attributeHasNoErrors("carta"))
-				.andExpect(model().attributeHasErrors("Pizza"))
-				.andExpect(status().isOk())
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/cuentas/{cuentaId}/pizzas/{pizzaId}/edit", TEST_CLIENTE_ID, TEST_PIZZA_ID)
-					.with(csrf())
-					.param("contador", "2")
-					.param("coste", "13")
-					.param("nombre", "Pizza2")
-					.param("tamano", "mini")
-					.param("tipoMasa", "extrafina"))
-			.andExpect(model().attributeHasNoErrors("cliente")) //cliente o cuenta?
-			.andExpect(model().attributeHasErrors("Pizza"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		/*mockMvc.perform(post("/ingredientes/{ingredienteId}/pizzas/{pizzaId}/edit", TEST_INGREDIENTE_ID, TEST_PIZZA_ID)
-					.with(csrf())
-					.param("contador", "2")
-					.param("coste", "13")
-					.param("nombre", "Pizza2")
-					.param("tamano", "mini")
-					.param("tipoMasa", "extrafina"))
-			.andExpect(model().attributeHasNoErrors("ingrediente"))
-			.andExpect(model().attributeHasErrors("Pizza"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));*/
 		
 	}
 
     @WithMockUser(value = "spring")
 	@Test
 	void testInitUpdateForm() throws Exception {
-		mockMvc.perform(get("/ofertas/{ofertaId}/pizzas/{pizzaId}/edit", TEST_OFERTA_ID, TEST_PIZZA_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("Pizza"))
+		mockMvc.perform(get("/pizzas/{pizzaId}/edit", TEST_PIZZA_ID))
+				.andExpect(status().isOk()).andExpect(model().attributeExists("pizza"))
 				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
 		
-		mockMvc.perform(get("/pedidos/{pedidoId}/pizzas/{pizzaId}/edit", TEST_PEDIDO_ID, TEST_PIZZA_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("Pizza"))
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(get("/cartas/{cartaId}/pizzas/{pizzaId}/edit", TEST_CARTA_ID, TEST_PIZZA_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("Pizza"))
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(get("/cuentas/{cuentaId}/pizzas/{pizzaId}/edit", TEST_CLIENTE_ID, TEST_PIZZA_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("Pizza"))
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-
-		/*mockMvc.perform(get("/ingredientes/{ingredienteId}/pizzas/{pizzaId}/edit", TEST_INGREDIENTE_ID, TEST_PIZZA_ID))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("Pizza"))
-				.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));*/
 	}
     
     @WithMockUser(value = "spring")
 	@Test
-	void testProcessUpdateFormSuccess() throws Exception {
-		mockMvc.perform(post("/ofertas/{ofertaId}/pizzas/{pizzaId}/edit", TEST_OFERTA_ID, TEST_PIZZA_ID)
+	void testProcessUpdatePizzaFormSuccess() throws Exception {
+		mockMvc.perform(post("/pizzas/{pizzaId}/edit", TEST_PIZZA_ID)
 				.with(csrf())
 				.param("contador", "2")
 				.param("coste", "13")
 				.param("nombre", "Pizza2")
 				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(status().is3xxRedirection())
-	.andExpect(view().name("redirect:/ofertas/{ofertaId}"));
+				.param("tipoMasa", "extrafina")
+				.param("ingredientes", "tomate"))
 
-		mockMvc.perform(post("/pedidos/{pedidoId}/pizzas/{pizzaId}/edit", TEST_PEDIDO_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
 	.andExpect(status().is3xxRedirection())
-	.andExpect(view().name("redirect:/pedidos/{pedidoId}"));
-		
-		mockMvc.perform(post("/cartas/{cartaId}/pizzas/{pizzaId}/edit", TEST_CARTA_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(status().is3xxRedirection())
-	.andExpect(view().name("redirect:/cartas/{cartaId}"));
-		
-		mockMvc.perform(post("/cuentas/{cuentaId}/pizzas/{pizzaId}/edit", TEST_OFERTA_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(status().is3xxRedirection())
-	.andExpect(view().name("redirect:/cuentas/{cuentaId}"));
+	.andExpect(view().name("redirect:/allPizzas"));
 
-		/*mockMvc.perform(post("/ingredientes/{ingredienteId}/pizzas/{pizzaId}/edit", TEST_PEDIDO_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(status().is3xxRedirection())
-	.andExpect(view().name("redirect:/ingredientes/{ingredienteId}"));*/
 	}
     
     @WithMockUser(value = "spring")
 	@Test
-	void testProcessUpdateFormHasErrors() throws Exception {
-		mockMvc.perform(post("/ofertas/{ofertaId}/pizzas/{pizzaId}/edit", TEST_OFERTA_ID, TEST_PIZZA_ID)
+	void testProcessUpdatePizzaFormHasErrors() throws Exception {
+		mockMvc.perform(post("/pizzas/{pizzaId}/edit", TEST_PIZZA_ID)
 				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
+				.param("contador", "ññññ")
+				.param("coste", "lllll")
 				.param("nombre", "Pizza2")
 				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(model().attributeHasNoErrors("oferta"))
-	.andExpect(model().attributeHasErrors("Pizza")).andExpect(status().isOk())
+				.param("tipoMasa", "extrafina")
+				.param("ingredientes", "tomate"))
+
+	.andExpect(status().isOk())
 	.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/pedidos/{pedidoId}/pizzas/{pizzaId}/edit", TEST_PEDIDO_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(model().attributeHasNoErrors("pedido"))
-	.andExpect(model().attributeHasErrors("Pizza")).andExpect(status().isOk())
-	.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/cartas/{cartaId}/pizzas/{pizzaId}/edit", TEST_CARTA_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(model().attributeHasNoErrors("carta"))
-	.andExpect(model().attributeHasErrors("Pizza")).andExpect(status().isOk())
-	.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		mockMvc.perform(post("/cuentas/{cuentaId}/pizzas/{pizzaId}/edit", TEST_CLIENTE_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(model().attributeHasNoErrors("cliente")) //cliente o cuenta
-	.andExpect(model().attributeHasErrors("Pizza")).andExpect(status().isOk())
-	.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));
-		
-		/*mockMvc.perform(post("/ingredientes/{ingredienteId}/pizzas/{pizzaId}/edit", TEST_INGREDIENTE_ID, TEST_PIZZA_ID)
-				.with(csrf())
-				.param("contador", "2")
-				.param("coste", "13")
-				.param("nombre", "Pizza2")
-				.param("tamano", "mini")
-				.param("tipoMasa", "extrafina"))
-	.andExpect(model().attributeHasNoErrors("ingrediente"))
-	.andExpect(model().attributeHasErrors("Pizza")).andExpect(status().isOk())
-	.andExpect(view().name("pizzas/createOrUpdatePizzaForm"));*/
+
 	}
-    
-    
-	@WithMockUser(value = "spring")
-    @Test
-void testShowPizzaListXml() throws Exception {
-	mockMvc.perform(get("/pizzas.xml").accept(MediaType.APPLICATION_XML)).andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_XML_VALUE))
-			.andExpect(content().node(hasXPath("/pizzas/pizzasList[id=3]/id")));
-}
 }
