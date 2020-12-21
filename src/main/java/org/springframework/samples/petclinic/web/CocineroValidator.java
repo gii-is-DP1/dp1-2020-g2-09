a5c5484b934dec23eb27debae348db054b56e2f8
Servando <servandofg12@gmail.com>
2020-12-21 17:50:37 +0100

    Arreglado los validadores de cliente, cocinero y repartidor

--- src/main/java/org/springframework/samples/petclinic/web/CocineroValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/CocineroValidator.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.petclinic.web;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.samples.petclinic.model.Cocina;
 import org.springframework.samples.petclinic.model.User;
@@ -28,7 +30,9 @@ public class CocineroValidator implements Validator {
 		LocalDate fechaNacimiento =cocinero.getFechaNacimiento();
 		String nombre = cocinero.getNombre();
 		Integer telefono = cocinero.getTelefono();
-		User nombreUsuario = cocinero.getUser();
+		User usuario = cocinero.getUser();
+		String nombreUsuario = usuario.getUsername();
+		String contraseña = usuario.getPassword();
 		
 		
 		//nombre
@@ -50,16 +54,39 @@ public class CocineroValidator implements Validator {
 			errors.rejectValue("fechaNacimiento", "La fecha no puede estar vacía","La fecha no puede estar vacía");
 		}
 				
+		// Patrón para validar el email
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matcher = pattern.matcher(email);
+
 		//email
 		if(email==null) {
 			errors.rejectValue("email", "El email no puede estar vacío",
 					"El email no puede estar vacío");
+		}else if(email.length()<12 || email.length()>30) {
+			errors.rejectValue("email", "El email es demasiado largo o corto",
+					"El email es demasiado largo o corto");
+		}else if(!matcher.find()) {
+			errors.rejectValue("email", "El email no es correcto",
+					"El email no es correcto");
 		}
 		//nombreUsuario
 		if(nombreUsuario==null) {
 			errors.rejectValue("user.username", "El nombre de usuario no puede estar vacío",
 					"El nombre de usuario no puede estar vacío");
+		}else if(nombreUsuario.length()<2 || nombreUsuario.length()>20) {
+			errors.rejectValue("user.username", "El nombre tiene que estar entre 2 y 20 caracteres",
+					"El nombre tiene que estar entre 2 y 20 caracteres");
 		} 
+		
+		if(contraseña==null) {
+			errors.rejectValue("user.password", "La contraseña de usuario no puede estar vacía",
+					"La contraseña de usuario no puede estar vacía");
+		}else if(contraseña.length()<2 || contraseña.length()>20) {
+			errors.rejectValue("user.password", "La contraseña de usuario tiene que estar entre 2 y 20 caracteres",
+					"La contraseña de usuario tiene que estar entre 2 y 20 caracteres");
+		}
 			
 						
 		//telefono
