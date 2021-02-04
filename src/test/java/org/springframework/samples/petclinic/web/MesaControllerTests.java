ad0438894617f1724527c696d11ae4ba4e44cda2
Jesús Roldán Cadena <jesrolcad@alum.us.es>
2021-02-04 14:17:07 +0100

    Tests de MesaController solucionados debido al versionado

--- src/test/java/org/springframework/samples/petclinic/web/MesaControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/MesaControllerTests.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Mesa;
+import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.service.MesaService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -42,7 +43,8 @@ public class MesaControllerTests {
 		Mesa mesa1 = new Mesa();
 		mesa1.setId(3);
 		mesa1.setCapacidad(5);
-		given(this.mesaService.findMesas()).willReturn(Lists.newArrayList(mesa1));
+		mesa1.setVersion(1);
+		given(this.mesaService.findById(TEST_MESA_ID)).willReturn(new Mesa());
 	}
 
 	@WithMockUser(value = "spring")
@@ -89,8 +91,7 @@ public class MesaControllerTests {
 	void testprocessUpdateMesaFormSuccess() throws Exception {
 		mockMvc.perform(post("/mesas/{mesaId}/edit", TEST_MESA_ID)
 				.with(csrf())
-				.param("capacidad", "3"))
-		
+				.param("capacidad", "5"))
 	.andExpect(status().is3xxRedirection())
 	.andExpect(view().name("redirect:/allMesas"));
 
@@ -102,7 +103,8 @@ public class MesaControllerTests {
 		mockMvc.perform(post("/mesas/{mesaId}/edit", TEST_MESA_ID)
 				.with(csrf())
 				.param("capacidad", "kbiygu"))
-
+		.andExpect(model().attributeHasErrors("mesa"))
+		.andExpect(model().attributeHasFieldErrors("mesa", "capacidad"))
 		.andExpect(status().isOk())
 		.andExpect(view().name("mesas/createOrUpdateMesaForm"));
     }
